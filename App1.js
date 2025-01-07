4c006c494f9c5f9049b0d781607d6f514cbb3ed2
minhp19 <minhphuong1924@gmail.com>
2025-01-07 10:21:32 +0700

    first commit

---
+++ App1.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.wrapper}>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, world</Text>
+    </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+      },
+
+  container: {
+    width: 100,  
+    height: 100, 
+    backgroundColor: 'blue',  
+    justifyContent: 'center', 
+    alignItems: 'center',      
+    display: 'flex',  
+  },
+  text: {
+    color: 'white',  
+    fontSize: 16     
+  }
+});
+
+export default App;
