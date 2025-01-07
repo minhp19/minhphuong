47f29cad8b55b09bc0d5438066e14b372c81190f
minhp19 <minhphuong1924@gmail.com>
2025-01-07 11:51:50 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { registerRootComponent } from 'expo';
 
-import Customcomponent from './Customcomponent';
+import  Flatlist from './Flatlist';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Customcomponent);
+registerRootComponent(Flatlist);
